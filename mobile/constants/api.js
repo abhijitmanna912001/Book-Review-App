49ab1d4dd9a77d5ba9870885254a784c1d9da080
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2026-03-06 21:46:28 +0530

    Api Deployment to Render completed and used in mobile

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,7 +1,1 @@
-import { Platform } from "react-native";
-
-const DEV_MACHINE = "http://localhost:3000";
-const ANDROID_EMULATOR = "http://10.0.2.2:3000";
-
-export const API_URL =
-  Platform.OS === "android" ? ANDROID_EMULATOR : DEV_MACHINE;
+export const API_URL = "https://book-store-app-qf67.onrender.com";
